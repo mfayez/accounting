4d7862bcb217f8232e4b33fb8693d5a3e689d658
mfayez <mahmoudfayez@gmail.com>
2021-08-14 01:50:19 +0000

    enable API tokens

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Item;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:sanctum')->get('/items', function (Request $request) {
+    return Item::all();
 });
--- webpack.mix.js
+++ webpack.mix.js
@@ -21,3 +21,12 @@ mix.js('resources/js/app.js', 'public/js').vue()
 if (mix.inProduction()) {
     mix.version();
 }
+
+mix.webpackConfig ({
+  plugins: [
+    new webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: false,
+      __VUE_PROD_DEVTOOLS__: false,
+    }),
+  ],
+})
