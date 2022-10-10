5071a581586857c4156c046600ba1b3cdec10ef3
j8UVZg4XXNkYT43hYT4X <mahmoudfayez@gmail.com>
2022-10-10 00:05:31 +0000

    Fix range of report date

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -62,7 +62,7 @@ class ReportsController extends Controller
 						    left outer join TaxTotal t5 on t5.invoice_id = t1.Id
 						where (t1.issuer_id = ? or ? = -1)
 							and   (t1.receiver_id = ? or ? = -1)
-							and t1.dateTimeIssued between ? and DATE_ADD(?, INTERVAL 1 DAY) and t1.status = 'Valid'
+							and t1.dateTimeIssued between ? and ? and t1.status = 'Valid'
 						group by t1.internalID, month(t1.dateTimeIssued), date(t1.dateTimeIssued), t4.name, t1.totalAmount, t1.Id";
 		$data = DB::select($strSqlStmt1, [$branchId, $branchId, $customerId, $customerId, $startDate, $endDate]);
 		return $data;
@@ -87,7 +87,7 @@ class ReportsController extends Controller
 						    left outer join TaxTotal t5 on t5.invoice_id = t1.Id
 						where (t1.issuer_id = ? or ? = -1)
 							and   (t1.receiver_id = ? or ? = -1)
-							and t1.dateTimeIssued between ? and DATE_ADD(?, INTERVAL 1 DAY) and t1.status = 'Valid'
+							and t1.dateTimeIssued between ? and ? and t1.status = 'Valid'
 						group by t1.Id, t1.internalID, month(t1.dateTimeIssued), date(t1.dateTimeIssued), t4.name, t1.totalAmount, 
 						t4.code, t1.totalSalesAmount, t1.documentType, t1.dateTimeIssued";
 		$data1 = DB::select($strSqlStmt1, [$branchId, $branchId, $customerId, $customerId, $startDate, $endDate]);
@@ -97,7 +97,7 @@ class ReportsController extends Controller
 						    inner join Value t7 on t7.Id = t2.unitValue_id
 						where (t1.issuer_id = ? or ? = -1)
 							and   (t1.receiver_id = ? or ? = -1)
-							and t1.dateTimeIssued between ? and DATE_ADD(?, INTERVAL 1 DAY) and t1.status = 'Valid'
+							and t1.dateTimeIssued between ? and ? and t1.status = 'Valid'
 						group by t1.Id, t2.description, t2.itemCode";
 		$data2 = DB::select($strSqlStmt2, [$branchId, $branchId, $customerId, $customerId, $startDate, $endDate]);
 		$items = array();
@@ -239,7 +239,7 @@ class ReportsController extends Controller
 						    left outer join TaxTotal t5 on t5.invoice_id = t1.Id
 						where (t1.issuer_id = ? or ? = -1)
 							and   (t1.receiver_id = ? or ? = -1)
-							and t1.dateTimeIssued between ? and DATE_ADD(?, INTERVAL 1 DAY) and t1.status = 'Valid'
+							and t1.dateTimeIssued between ? and ? and t1.status = 'Valid'
 						group by t1.Id, t1.internalID, month(t1.dateTimeIssued), date(t1.dateTimeIssued), t4.name, t1.totalAmount, 
 						t4.code, t1.totalSalesAmount, t1.documentType, t1.dateTimeIssued";
 		$data1 = DB::select($strSqlStmt1, [$branchId, $branchId, $customerId, $customerId, $startDate, $endDate]);
@@ -249,7 +249,7 @@ class ReportsController extends Controller
 						    inner join Value t7 on t7.Id = t2.unitValue_id
 						where (t1.issuer_id = ? or ? = -1)
 							and   (t1.receiver_id = ? or ? = -1)
-							and t1.dateTimeIssued between ? and DATE_ADD(?, INTERVAL 1 DAY) and t1.status = 'Valid'
+							and t1.dateTimeIssued between ? and ? and t1.status = 'Valid'
 						group by t1.Id, t2.description, t2.itemCode";
 		$data2 = DB::select($strSqlStmt2, [$branchId, $branchId, $customerId, $customerId, $startDate, $endDate]);
 		$items = array();
@@ -379,7 +379,7 @@ class ReportsController extends Controller
 		$strSqlStmt1 = "select t1.internalID as Id, month(t1.dateTimeIssued) as Month, date(t1.dateTimeIssued) as Date, 
 							t1.issuerName as Seller, t1.issuerId as SellerTaxId, t1.totalSales as Sales, t1.netAmount as Net, t1.total as Total
 						from ETAInvoices t1 
-						where t1.dateTimeIssued between ? and DATE_ADD(?, INTERVAL 1 DAY) and t1.status = 'Valid'";
+						where t1.dateTimeIssued between ? and ? and t1.status = 'Valid'";
 		$data = DB::select($strSqlStmt1, [$startDate, $endDate]);
 		return $data;
 	}
@@ -391,7 +391,7 @@ class ReportsController extends Controller
 		$strSqlStmt1 = "select t1.internalID as Id, month(t1.dateTimeIssued) as Month, date(t1.dateTimeIssued) as Date, 
 							t1.issuerName as Seller, t1.issuerId as SellerTaxId, t1.totalSales as Sales, t1.netAmount as Net, t1.total as Total
 						from ETAInvoices t1 
-						where t1.dateTimeIssued between ? and DATE_ADD(?, INTERVAL 1 DAY) and t1.status = 'Valid'";
+						where t1.dateTimeIssued between ? and ? and t1.status = 'Valid'";
 		$data = DB::select($strSqlStmt1, [$startDate, $endDate]);
 		
 		//render excel file now
@@ -435,7 +435,7 @@ class ReportsController extends Controller
 							sum(t5.amount) as TaxTotal, sum(t1.totalSalesAmount) as SalesTotal, sum(t1.totalAmount) as Total
 						from Invoice t1 inner join Issuer t3 on t3.Id = t1.issuer_id
 							left outer join TaxTotal t5 on t5.invoice_id = t1.Id
-						where t1.dateTimeIssued between ? and DATE_ADD(?, INTERVAL 1 DAY) and t1.status = 'Valid'
+						where t1.dateTimeIssued between ? and ? and t1.status = 'Valid'
 						group by t3.Id, t3.name";
 		$data = DB::select($strSqlStmt1, [$startDate, $endDate]);
 		return $data;
@@ -449,7 +449,7 @@ class ReportsController extends Controller
 							sum(t5.amount) as TaxTotal, sum(t1.totalSalesAmount) as SalesTotal, sum(t1.totalAmount) as Total
 						from Invoice t1 inner join Issuer t3 on t3.Id = t1.issuer_id
 							left outer join TaxTotal t5 on t5.invoice_id = t1.Id
-						where t1.dateTimeIssued between ? and DATE_ADD(?, INTERVAL 1 DAY) and t1.status = 'Valid'
+						where t1.dateTimeIssued between ? and ? and t1.status = 'Valid'
 						group by t3.Id, t3.name";
 		$data = DB::select($strSqlStmt1, [$startDate, $endDate]);
 		
@@ -498,7 +498,7 @@ class ReportsController extends Controller
 						from Invoice t1 inner join Receiver t4 on t4.Id = t1.receiver_id
 							left outer join TaxTotal t5 on t5.invoice_id = t1.Id
 						where (t1.issuer_id = ? or ? = -1)
-						and t1.dateTimeIssued between ? and DATE_ADD(?, INTERVAL 1 DAY) and t1.status = 'Valid'
+						and t1.dateTimeIssued between ? and ? and t1.status = 'Valid'
 						group by t4.receiver_id, t4.name";
 		$data = DB::select($strSqlStmt1, [$branchId, $branchId, $startDate, $endDate]);
 		return $data;
@@ -514,7 +514,7 @@ class ReportsController extends Controller
 						from Invoice t1 inner join Receiver t4 on t4.Id = t1.receiver_id
 							left outer join TaxTotal t5 on t5.invoice_id = t1.Id
 						where (t1.issuer_id = ? or ? = -1)
-						and t1.dateTimeIssued between ? and DATE_ADD(?, INTERVAL 1 DAY) and t1.status = 'Valid'
+						and t1.dateTimeIssued between ? and ? and t1.status = 'Valid'
 						group by t4.receiver_id, t4.name, t4.code";
 		$data = DB::select($strSqlStmt1, [$branchId, $branchId, $startDate, $endDate]);
 		
